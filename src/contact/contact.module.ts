c8ddd22f76e6ffa3f714bd174d71205450bba95e
ShohsultonDev <shohsulton.198@gmail.com>
2025-05-11 02:27:12 +0500

    test

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -5,9 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
 import { Contact, ContactSchema } from 'src/models/contact.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
+    AuthModule
+  ],
   controllers: [ContactController],
   providers: [ContactService],
 })
